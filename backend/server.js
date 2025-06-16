738cdb09b09c65c336b98cb08e37c4d008af7300
AnvithBhagavath <160594625+AnvithBhagavath@users.noreply.github.com>
2025-06-16 12:40:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -41,9 +41,13 @@ const port = process.env.PORT || 3000;
 // MySQL Connection
 const db = mysql.createConnection({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // Include port
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: true // or false based on your Aiven SSL config
+    }
 });
 
 // Connect to MySQL
